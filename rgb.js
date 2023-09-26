0b9655af8b427815e3abf9eb0801a4ec36fe2a05
A M Rizal <34767112+amrizal94@users.noreply.github.com>
2023-09-26 12:16:09 +0700

    add documentation on rgb.js

--- rgb.js
+++ rgb.js
@@ -1,13 +1,28 @@
-function rgb(r, g, b) {
-  r <= 0 ? (r = "00") : r > 255 ? (r = "FF") : (r = r.toString(16).toUpperCase().padStart(2,0));
-  g <= 0 ? (g = "00") : g > 255 ? (g = "FF") : (g = g.toString(16).toUpperCase().padStart(2,0));
-  b <= 0 ? (b = "00") : b > 255 ? (b = "FF") : (b = b.toString(16).toUpperCase().padStart(2,0));
+/**
+ * 
+ * @param  {...any} args array of number arguments
+ * The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned. Valid decimal values for RGB are 0 - 255. Any values that fall out of that range must be rounded to the closest valid value.
+ * Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+ * 
+ * @returns string hexadecimal representation
+ * Examples (input --> output):
+ * 
+ * 255, 255, 255 --> "FFFFFF"
+ * 255, 255, 300 --> "FFFFFF"
+ * 0, 0, 0       --> "000000"
+ * 148, 0, 211   --> "9400D3"
+ */
 
-  return`${r}${g}${b}`;
-}
+const rgb = (...args) =>
+  args.map(arg => Math.max(Math.min(arg, 255), 0)
+    .toString(16)
+    .padStart(2, '0')
+    .toUpperCase())
+    .join('');
+
 
 console.log(rgb(0, 0, 0), "000000");
 console.log(rgb(0, 0, -20), "000000");
 console.log(rgb(300, 255, 255), "FFFFFF");
 console.log(rgb(173, 255, 47), "ADFF2F");
-console.log(rgb(20,299,9), "14FF09"); 
+console.log(rgb(20, 299, 9), "14FF09"); 
